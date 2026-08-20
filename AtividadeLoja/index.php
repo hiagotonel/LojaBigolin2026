40b120725d9f3a7652b0f10932f2b72667faa706
Noxious616 <hiagotonel@gmail.com>
2026-08-20 20:45:22 -0300

    Pequenos detalhes

--- AtividadeLoja/index.php
+++ AtividadeLoja/index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Loja</title>
+    <title>Loja Bigolin</title>
 </head>
 <body>
     <?php require "pages/header.php"; ?>
     <main>
-        <h1>O que deseja fazer hoje?</h1>
+        <h1>Bem-vindo a Loja Bigolin!</h1>
+        <h3>O que deseja fazer hoje?</h3>
 
         <ul>
             <li><a href="pages/inserir.php">Inserir, excluir e atualizar Dados</a></li>
--- AtividadeLoja/pages/header.php
+++ AtividadeLoja/pages/header.php
@@ -1,11 +1,11 @@
 <header>
     <ul>
-        <li><a href="/AulaLinguagensParaInternet/AtividadeLoja/index.php">Início</a></li>
-        <li><a href="/AulaLinguagensParaInternet/AtividadeLoja/select/select_produto.php">Produtos</a></li>
-        <li><a href="/AulaLinguagensParaInternet/AtividadeLoja/select/select_cliente.php">Clientes</a></li>
-        <li><a href="/AulaLinguagensParaInternet/AtividadeLoja/select/select_pedido.php">Pedidos</a></li>
-        <li><a href="/AulaLinguagensParaInternet/AtividadeLoja/select/select_estoque.php">Estoque</a></li>
-        <li><a href="/AulaLinguagensParaInternet/AtividadeLoja/select/select_marca.php">Marcas</a></li>
-        <li><a href="/AulaLinguagensParaInternet/AtividadeLoja/select/select_setor.php">Setores</a></li>
+        <li><a href="../index.php">Início</a></li>
+        <li><a href="../select/select_produto.php">Produtos</a></li>
+        <li><a href="../select/select_cliente.php">Clientes</a></li>
+        <li><a href="../select/select_pedido.php">Pedidos</a></li>
+        <li><a href="../select/select_estoque.php">Estoque</a></li>
+        <li><a href="../select/select_marca.php">Marcas</a></li>
+        <li><a href="../select/select_setor.php">Setores</a></li>
     </ul>
 </header>
