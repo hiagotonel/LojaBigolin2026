34777e003c318a14f487529a20703e56817c826a
Noxious616 <hiagotonel@gmail.com>
2026-08-20 22:06:13 -0300

    troca de html minima

--- pages/insert.php
+++ pages/insert.php
@@ -4,30 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles.css">
-    <title>Exibir Dados</title>
+    <title>Inserir Dados</title>
 </head>
 <body>
     <?php require "header.php"; ?>
     <main>
-    <h1>O que deseja exibir?</h1>
+    <h1>Que dados deseja inserir?</h1>
     <ul>
         <li>
-            <a href="../select/select_produto.php">Produto</a>
+            <a href="../forms/form_produto.php">Produto</a>
         </li>
         <li>
-            <a href="../select/select_cliente.php">Cliente</a>
+            <a href="../forms/form_cliente.php">Cliente</a>
         </li>
         <li>
-            <a href="../select/select_pedido.php">Pedido</a>
+            <a href="../forms/form_pedido.php">Pedido</a>
         </li>
         <li>
-            <a href="../select/select_estoque.php">Estoque</a>
+            <a href="../forms/form_estoque.php">Estoque</a>
         </li>
         <li>
-            <a href="../select/select_marca.php">Marca</a>
+            <a href="../forms/form_marca.php">Marca</a>
         </li>
         <li>
-            <a href="../select/select_setor.php">Setor</a>
+            <a href="../forms/form_setor.php">Setor</a>
         </li>
     </ul>
 </body>
--- pages/select.php
+++ pages/select.php
@@ -4,30 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles.css">
-    <title>Inserir Dados</title>
+    <title>Exibir Dados</title>
 </head>
 <body>
     <?php require "header.php"; ?>
     <main>
-    <h1>O que deseja inserir?</h1>
+    <h1>Que dados você quer consultar?</h1>
     <ul>
         <li>
-            <a href="../forms/form_produto.php">Produto</a>
+            <a href="../select/select_produto.php">Produto</a>
         </li>
         <li>
-            <a href="../forms/form_cliente.php">Cliente</a>
+            <a href="../select/select_cliente.php">Cliente</a>
         </li>
         <li>
-            <a href="../forms/form_pedido.php">Pedido</a>
+            <a href="../select/select_pedido.php">Pedido</a>
         </li>
         <li>
-            <a href="../forms/form_estoque.php">Estoque</a>
+            <a href="../select/select_estoque.php">Estoque</a>
         </li>
         <li>
-            <a href="../forms/form_marca.php">Marca</a>
+            <a href="../select/select_marca.php">Marca</a>
         </li>
         <li>
-            <a href="../forms/form_setor.php">Setor</a>
+            <a href="../select/select_setor.php">Setor</a>
         </li>
     </ul>
 </body>
