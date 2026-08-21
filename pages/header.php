492ba09233c9c4b9b4a5150bdc05b8d1fc5c7273
Noxious616 <hiagotonel@gmail.com>
2026-08-20 21:01:52 -0300

    tirei a pasta pai e fiz uma mudança de css

--- pages/header.php
+++ pages/header.php
@@ -1,6 +1,6 @@
 <header>
     <ul>
-        <li><a href="../index.php">Início</a></li>
+        <li><a href="index.php">Início</a></li>
         <li><a href="../select/select_produto.php">Produtos</a></li>
         <li><a href="../select/select_cliente.php">Clientes</a></li>
         <li><a href="../select/select_pedido.php">Pedidos</a></li>
