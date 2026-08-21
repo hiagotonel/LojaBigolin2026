f5e799562b5af794878f846dded1cd62a7aef597
hiagotonel <hiagotonel@gmail.com>
2026-08-21 13:42:04 -0300

    troca dos caminhos

--- pages/header.php
+++ pages/header.php
@@ -1,11 +1,11 @@
 <header>
     <ul>
-        <li><a href="/index.php">Início</a></li>
-        <li><a href="/select/select_produto.php">Produtos</a></li>
-        <li><a href="/select/select_cliente.php">Clientes</a></li>
-        <li><a href="/select/select_pedido.php">Pedidos</a></li>
-        <li><a href="/select/select_estoque.php">Estoque</a></li>
-        <li><a href="/select/select_marca.php">Marcas</a></li>
-        <li><a href="/select/select_setor.php">Setores</a></li>
+        <li><a href="../index.php">Início</a></li>
+        <li><a href="../select/select_produto.php">Produtos</a></li>
+        <li><a href="../select/select_cliente.php">Clientes</a></li>
+        <li><a href="../select/select_pedido.php">Pedidos</a></li>
+        <li><a href="../select/select_estoque.php">Estoque</a></li>
+        <li><a href="../select/select_marca.php">Marcas</a></li>
+        <li><a href="../select/select_setor.php">Setores</a></li>
     </ul>
 </header>
